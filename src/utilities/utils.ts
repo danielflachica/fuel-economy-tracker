fd1fb78203e4339d591876a1c085fcd8ad203bad
Daniel Lachica <daniel.lachica82@gmail.com>
2026-06-25 15:16:49 +0800

    Update validation rules

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,8 +1,8 @@
 export const numberRules = {
-  required: "This field is required.",
+  required: "This field is required",
   validate: (val: string) => {
-    if (isNaN(Number(val))) return "Must be a number.";
-    if (Number(val) <= 0) return "Must be greater than 0.";
+    if (isNaN(Number(val))) return "Must be a number";
+    if (Number(val) <= 0) return "Must be greater than 0";
     return true;
   },
 };
